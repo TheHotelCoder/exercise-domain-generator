f73ad1e29286443765c6cb34db954af0b8548b29
Diana Ware <dianasofia.ware@gmail.com>
2021-04-17 07:34:12 +0000

    Finished exercise

--- app.js
+++ app.js
@@ -1,2 +1,18 @@
-var test = null
-var test2 = "hello"
+let pronoun = ['the','our'];
+let adj = ['great', 'big' ];
+let noun = ['jogger','racoon'];
+let extension = ['.com', '.net', '.us', '.io']
+
+
+
+for(let x = 0; x < pronoun.length; x++) {
+    for(let y = 0; y < adj.length; y++) {
+        for (let z = 0; z < noun.length; z++) {
+            for(let a = 0; a < extension.length; a++) {
+                console.log( `${pronoun[x]}${adj[y]}${noun[z]}${extension[a]}` )
+            }
+            
+        }
+    }
+}
+
